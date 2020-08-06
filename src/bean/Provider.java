de5a6743d165aae58c83ce3fbf263e2bb755ea2d
안명훈 <@DESKTOP-MD0V7U1>
2020-08-06 23:34:35 +0900

    go

---
+++ src/bean/Provider.java
@@ -0,0 +1,9 @@
+package bean;
+
+public interface Provider {
+	String DRIVER = "com.mysql.jdbc.Driver";
+	String CONNECTION_URL = "jdbc:mysql://www.db4free.net:3306/mymeta";
+	String USERNAME = "mymeta";
+	String PASSWORD = "mt2020!!";
+
+}
